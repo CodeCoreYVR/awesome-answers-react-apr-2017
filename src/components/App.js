f061cd3d0cc7ea7612b4d26dd3dec563743f77b4
Steve Godin <steve.god@gmail.com>
2017-06-29 15:08:36 -0700

    Add navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 import QuestionsIndex from './QuestionsIndex';
 import QuestionsShow from './QuestionsShow'
 
 const App = () => (
   <Router>
-    <div>
+    <div className="App">
+      <nav>
+        <Link to='/'>Home</Link> | <Link to='/questions'>Questions</Link>
+      </nav>
+      <hr />
+
       <Route exact path="/" component={QuestionsIndex} />
+      <Route exact path="/questions" component={QuestionsIndex} />
       <Route path="/questions/:id" component={QuestionsShow} />
     </div>
   </Router>
